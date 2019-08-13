e6b6b58cdc33b91e16187052f7834b810ddb537e
newms34 <newms3450@gmail.com>
2019-08-13 14:02:47 -0400

    woops

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
+import {
+    tinyMsg
+} from './tinyBlurbs.js'
 const q = document.querySelector.bind(document);
 const dialog = q('#taimiDialog'),
     talk = q('#talkBtn');
-
+// console.log(tinyMsg)
 // not sure how we can get this to play, as autoplay is no longer enabled in chrome
 const sound = new Howl({
     src: ['./SillyChickenV2.mp3'],
@@ -47,7 +50,7 @@ document.getElementById('myBtn').addEventListener('click', (e) => {
 })
 
 //Count real jumps!!!
-startJumping = () => {
+const startJumping = () => {
     const jumpinTaimi = document.getElementById('JumpinTaimi');
     let localJumps = 0;
     jumpinTaimi.play();
@@ -60,14 +63,16 @@ startJumping = () => {
     }
 }
 let currDiagCont = 0,
-    message = null;
+    currTinyCount = 0,
+    message = null,
+    isTiny = false;
 const dialogOpts = {
     closes: ['I really don\'t have the time right now Taimi.', 'That\'s great, Taimi! Talk to you soon!', 'Got it, Taimi. Over and out.'],
     continues: ['Uh huh...', 'Go on...', 'And?', 'So...', 'You\'re saying...']
 }
 
 const taimiSpeak = () => {
-    talk.hidden=true;
+    talk.hidden = true;
     fetch('/getMarkov?sents=' + Math.ceil(Math.random() * 5), {
         cache: 'no-store'
     }).then(q => {
@@ -78,6 +83,9 @@ const taimiSpeak = () => {
             taimi: r.hasContinue ? r.chunks[currDiagCont].join(' ') : r.string,
             chunks: r.chunks,
             replies: [{
+                icon: 'back',
+                type: 'change'
+            }, {
                 icon: 'exit',
                 type: 'close'
             }]
@@ -88,18 +96,28 @@ const taimiSpeak = () => {
                 type: 'continue'
             })
         }
-        showDialog(message);
+        showDialog();
     })
 }
 
 
 const closeDialog = () => {
-    talk.hidden=false;
+    talk.hidden = false;
     dialog.hidden = true;
 }
 
+const changeDialog = () => {
+    isTiny = !isTiny;
+    if (isTiny) {
+        currTinyCount = 0;
+    } else {
+        currDiagCont = 0;
+    }
+    showDialog();
+}
+
 const continueDialog = () => {
-    talk.hidden=false;
+    talk.hidden = false;
     currDiagCont++;
     message.taimi = message.chunks[currDiagCont].join(' ');
     if (currDiagCont >= message.chunks.length - 1) {
@@ -107,14 +125,31 @@ const continueDialog = () => {
     }
     showDialog(message);
 }
+
 q('#dialogName').addEventListener('click', closeDialog);
-talk.addEventListener('click',taimiSpeak)
+talk.addEventListener('click', taimiSpeak)
 
 
-const showDialog = (message) => {
-    q('#dialogText').innerText = message.taimi;
-    let replies = message.replies.map(r => {
-        const msg = dialogOpts[r.type+'s'][Math.floor(Math.random()*dialogOpts[r.type+'s'].length)];
+const showDialog = () => {
+    //function just displays a particular dialog.
+    const mess = isTiny ? tinyMsg : message;
+    q('#dialogText').innerText = mess.taimi;
+    let replies = mess.replies.map(r => {
+        let msg = null;
+        if (r.type != 'change' && !isTiny) {
+            // this reply is just one of our normal 'continue' or 'exit' replies for auto-genned dialog
+            msg = dialogOpts[r.type + 's'][Math.floor(Math.random() * dialogOpts[r.type + 's'].length)];
+        } else if (r.type == 'change' && !isTiny) {
+            msg = 'Can you tell me about [TINY]?';
+        } else if (r.type == 'change' && isTiny) {
+            msg = 'You were saying earlier...';
+        } else {
+            if (r.type == 'continue') {
+                msg = 'Tell me more!'
+            } else if (r.type == 'exit') {
+                msg = "Thanks, but I'm not interested.";
+            }
+        }
         return `<div class="replymessage" onclick="${r.type}Dialog()">
             <img width="44" src="img/${r.icon}.png"/>${msg}
         </div>`
